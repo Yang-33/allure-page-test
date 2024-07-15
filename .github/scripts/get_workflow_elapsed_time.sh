6fccd582d3fef2c6fc0f6f4cbe87d0685a746be4
Yuta Kasai <kasai.yuta0810@gmail.com>
2024-07-15 21:16:39 +0900

    use current time instead of createdAt

--- .github/scripts/get_workflow_elapsed_time.sh
+++ .github/scripts/get_workflow_elapsed_time.sh
@@ -4,23 +4,23 @@ RUN_ID=$1
 
 echo "Run ID: $RUN_ID"
 
-STARTED_AT=$(gh run view $RUN_ID --json startedAt | jq -r '.startedAt')
-UPDATED_AT=$(gh run view $RUN_ID --json updatedAt | jq -r '.updatedAt')
+CURRENT_TIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ") ## `updatedAt` doesn't work as expected.
+echo "Current Time: $CURRENT_TIME"
 
+STARTED_AT=$(gh run view $RUN_ID --json startedAt | jq -r '.startedAt')
 echo "Started At: $STARTED_AT"
-echo "Updated At: $UPDATED_AT"
 
 STARTED_SEC=$(date -d "$STARTED_AT" +%s)
-UPDATED_SEC=$(date -d "$UPDATED_AT" +%s)
+CURRENT_SEC=$(date -d "$CURRENT_TIME" +%s)
 
 echo "Started Seconds: $STARTED_SEC"
-echo "Updated Seconds: $UPDATED_SEC"
+echo "Current Seconds: $CURRENT_SEC"
 
-ELAPSED_SECONDS=$(($UPDATED_SEC - $STARTED_SEC))
+ELAPSED_SECONDS=$(($CURRENT_SEC - $STARTED_SEC))
 ELAPSED_MINUTES=$(($ELAPSED_SECONDS / 60))
 REMAINING_SECONDS=$(($ELAPSED_SECONDS % 60))
 
 echo "Elapsed time: $ELAPSED_MINUTES minutes and $REMAINING_SECONDS seconds"
 
 ELAPSED_TIME="${ELAPSED_MINUTES} min ${REMAINING_SECONDS} sec"
-echo "ELAPSED_TIME=$ELAPSED_TIME" >> $GITHUB_OUTPUT
+echo "elapsed_time=$ELAPSED_TIME" >> $GITHUB_OUTPUT
