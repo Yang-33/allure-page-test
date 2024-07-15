ca342fa0e65f857329de86a60268156021e4bf7f
Yuta Kasai <kasai.yuta0810@gmail.com>
2024-07-15 22:35:51 +0900

    clean up

--- .github/scripts/get_workflow_elapsed_time.sh
+++ .github/scripts/get_workflow_elapsed_time.sh
@@ -2,7 +2,7 @@
 
 STARTED_AT=$1
 
-echo "Started At: $STARTED_AT"
+echo "Started At: $STARTED_AT" ## `startedAt` from github api contains meaning less time by concurrency restrictions.
 CURRENT_TIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ") ## `updatedAt` doesn't work as expected.
 echo "Current Time: $CURRENT_TIME"
 
