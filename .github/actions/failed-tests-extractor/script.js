eb9e764bcefe2e5454fb0346598d269dcc995180
Yuta Kasai <kasai.yuta0810@gmail.com>
2024-07-15 16:21:05 +0900

    add slack format

--- .github/actions/failed-tests-extractor/script.js
+++ .github/actions/failed-tests-extractor/script.js
@@ -53,6 +53,8 @@ async function summarizeResults() {
     });
 
     totalSummary.detailedFailures = totalSummary.detailedFailures.slice(0, maxEntries);
+    // totalSummary.detailedFailuresWithEscape = JSON.stringify(totalSummary.detailedFailures).replace(/"/g, '\\"');
+    totalSummary.detailedFailuresForSlack = totalSummary.detailedFailures.map(item => `\n• ${item.name} - ${item.time}`);
     totalSummary.status = totalSummary.totalFailed > 0 ? 'Failure' : 'Success';
 
     console.log(JSON.stringify(totalSummary));
